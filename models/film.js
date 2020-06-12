c55e06b160e52e68f31f226eaae6723d8f0aa149
Tommy Pranoto <tommyprn8@gmail.com>
2020-06-12 15:05:18 +0700

    4.Film

---
+++ models/film.js
@@ -0,0 +1,18 @@
+'use strict';
+module.exports = (sequelize, DataTypes) => {
+  const Film = sequelize.define('Film', {
+    title: DataTypes.STRING,
+    thumbnail: DataTypes.STRING,
+    year: DataTypes.INTEGER,
+    categoryId: DataTypes.INTEGER,
+    description: DataTypes.STRING
+  }, {});
+  Film.associate = function(models) {
+    Film.belongsTo(models.Category, {
+      foreignKey: {
+        name: "categoryId",
+      },
+    });
+  };
+  return Film;
+};
